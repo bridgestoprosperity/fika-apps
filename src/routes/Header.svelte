0fb45b6294a493f7accf8dd519f47baf0dee9061
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-11-19 15:06:34 -0800

    adding daisy ui theme

---
+++ src/routes/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<header>
+	<div class="navbar bg-base-100">
+		<a class="btn btn-ghost text-xl">Bridges to Prosperity</a>
+	</div>
+</header>
+
+<style>
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,8 @@ export default {
 		extend: {}
 	},
 
-	plugins: [typography]
+	plugins: [typography, require('daisyui')],
+	daisyui: {
+		themes: [ 'light', 'dark', 'cupcake', 'bumblebee', 'emerald', 'corporate', 'synthwave', 'retro', 'cyberpunk', 'valentine', 'halloween', 'garden', 'forest', 'aqua', 'lofi', 'pastel', 'fantasy', 'wireframe', 'black', 'luxury', 'dracula', 'cmyk', 'autumn', 'business', 'acid', 'lemonade', 'night', 'coffee', 'winter', 'dim', 'nord', 'sunset',{ mytheme: { "primary": "#a991f7", "secondary": "#f6d860", "accent": "#37cdbe", "neutral": "#3d4451", "base-100": "#ffffff", }, }, ]
+	}
 };
