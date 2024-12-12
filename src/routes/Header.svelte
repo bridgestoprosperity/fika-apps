99ccf65bf72fb5850806b68b99135932c11da8ae
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-12-12 14:02:08 -0800

    updating after splitting up waternet into separate components

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,9 +2,6 @@
 </script>
 
 <header>
-	<div class="navbar bg-base-100">
-		<a class="btn btn-ghost text-xl">Bridges to Prosperity</a>
-	</div>
 </header>
 
 <style>
--- src/routes/maptest/+page.svelte
+++ src/routes/maptest/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-	import PmTilesMap from '$lib/components/PmTilesMap.svelte';
+
 
 </script>
 
 <div class="relative h-screen w-full">
-	<PmTilesMap />
+
 </div>
