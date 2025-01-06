46a228afe18001dfde980461bff173c49f7c4fd3
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2025-01-06 14:23:33 -0800

    updating metadata for waternet

--- src/routes/waternet/+page.svelte
+++ src/routes/waternet/+page.svelte
@@ -4,6 +4,15 @@
     import SideNav from '$lib/components/SideNav.svelte';
 </script>
 
+<svelte:head>
+    <title>Waternet Explorer</title>
+    <meta name="description" content="An application to explore waterways data created with Waternet" />
+    <meta property="og:image" content="https://apps.fikamap.com/waternet-magma-5.png" />
+    <meta name="twitter:image" content="https://apps.fikamap.com/waternet-magma-5.png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="726" />
+</svelte:head>
+
 <div class="relative h-screen w-full overflow-hidden">
     <!-- Map container -->
     <div class="absolute inset-0">
