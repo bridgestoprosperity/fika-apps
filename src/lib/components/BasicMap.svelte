55dc10b7f59d4ca42e80b388aca610448167f349
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2026-06-12 14:38:51 -0700

    Update Mapbox token in BasicMap.svelte

--- src/lib/components/BasicMap.svelte
+++ src/lib/components/BasicMap.svelte
@@ -3,7 +3,7 @@
 	import mapboxgl from 'mapbox-gl';
 
 	const MAPBOX_TOKEN =
-		'pk.eyJ1IjoiYnJpZGdlc3RvcHJvc3Blcml0eSIsImEiOiJjajRpd2sxeGQwMjU5MnhxajJkNzZnODZtIn0.UrOwxq6A1Zl2yvwzYxBudQ';
+		'pk.eyJ1IjoiYnJpZGdlc3RvcHJvc3Blcml0eSIsImEiOiJjbG51aHV6MDEwZDI1Mmx0MzhqcHFvb3BuIn0.wgSlMp8cyuIYVMvj_p-Ojg';
 
 	let mapContainer;
 	let map;
